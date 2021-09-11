edfa1565389a3c923badc49388896e343ffb651a
sceptre520 <denysshowdev@gmail.com>
2021-09-11 23:59:32 +0300

    rust2

--- src/program-rust/src/lib.rs
+++ src/program-rust/src/lib.rs
@@ -5,7 +5,7 @@
 
 pub mod error;
 pub mod instruction;
-// pub mod native_mint;
+pub mod native_mint;
 pub mod processor;
 pub mod state;
 
@@ -16,23 +16,23 @@ mod entrypoint;
 pub use solana_program;
 use solana_program::{entrypoint::ProgramResult, program_error::ProgramError, pubkey::Pubkey};
 
-// /// Convert the UI representation of a token amount (using the decimals field defined in its mint)
-// /// to the raw amount
-// pub fn ui_amount_to_amount(ui_amount: f64, decimals: u8) -> u64 {
-//     (ui_amount * 10_usize.pow(decimals as u32) as f64) as u64
-// }
+/// Convert the UI representation of a token amount (using the decimals field defined in its mint)
+/// to the raw amount
+pub fn ui_amount_to_amount(ui_amount: f64, decimals: u8) -> u64 {
+    (ui_amount * 10_usize.pow(decimals as u32) as f64) as u64
+}
 
-// /// Convert a raw amount to its UI representation (using the decimals field defined in its mint)
-// pub fn amount_to_ui_amount(amount: u64, decimals: u8) -> f64 {
-//     amount as f64 / 10_usize.pow(decimals as u32) as f64
-// }
+/// Convert a raw amount to its UI representation (using the decimals field defined in its mint)
+pub fn amount_to_ui_amount(amount: u64, decimals: u8) -> f64 {
+    amount as f64 / 10_usize.pow(decimals as u32) as f64
+}
 
-// solana_program::declare_id!("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA");
+solana_program::declare_id!("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA");
 
-// /// Checks that the supplied program ID is the correct one for SPL-token
-// pub fn check_program_account(spl_token_program_id: &Pubkey) -> ProgramResult {
-//     if spl_token_program_id != &id() {
-//         return Err(ProgramError::IncorrectProgramId);
-//     }
-//     Ok(())
-// }
+/// Checks that the supplied program ID is the correct one for SPL-token
+pub fn check_program_account(spl_token_program_id: &Pubkey) -> ProgramResult {
+    if spl_token_program_id != &id() {
+        return Err(ProgramError::IncorrectProgramId);
+    }
+    Ok(())
+}
